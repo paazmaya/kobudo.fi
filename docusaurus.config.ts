1ac232daf02df63bb4659cea0028dea9ec7452ea
Juga Paazmaya <paazmaya@yahoo.com>
2026-03-13 23:04:12 +0200

    Use rem for sizing in CSS and Microsoft Clarity to see what is populat

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -88,6 +88,15 @@ const config: Config = {
         content: "https://kobudo.fi/img/hero-dojo-wall.png",
       },
     },
+    {
+      tagName: "script",
+      attributes: { type: "text/javascript" },
+      innerHTML: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "vvat8zr3wa");`,
+    },
   ],
   presets: [
     [
